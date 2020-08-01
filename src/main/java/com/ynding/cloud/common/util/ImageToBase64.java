f37a897477667865ee2f07cdfb656a4fd597137e
yaha <913690560@qq.com>
2020-08-01 20:16:52 +0800

    Initial commit

---
+++ src/main/java/com/ynding/cloud/common/util/ImageToBase64.java
@@ -0,0 +1,55 @@
+package com.ynding.cloud.common.util;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
+public class ImageToBase64 {
+    /**
+     * 将网络图片转换成base64码
+     * @param netImagePath
+     * @return
+     */
+    private static String NetImageToBase64(String netImagePath) {
+
+//        netImagePath = "http://res.microsoul.com/" + netImagePath;//测试环境
+        //netImagePath = "http://image.aipaas.com" + netImagePath;//正式环境
+
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+
+        try {
+            URL url = new URL(netImagePath);
+
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+            InputStream is = conn.getInputStream();
+
+            BufferedImage bi = ImageIO.read(is);
+            ImageIO.write(bi,"jpg",data);
+
+            is.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Base64.getEncoder().encodeToString(data.toByteArray()).replaceAll("/r/n","");
+    }
+
+
+    public static void main(String[] args) {
+
+        String base64 = NetImageToBase64("http://chuantu.xyz/t6/702/1565918522x3752237043.jpg");
+
+        System.out.println(base64);
+    }
+
+
+}
---
+++ src/main/java/com/ynding/cloud/common/util/PropertiesUtil.java
@@ -0,0 +1,78 @@
+package com.ynding.cloud.common.util;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Slf4j
+public class PropertiesUtil {
+
+    private static Properties props;
+
+    static{
+        loadProps();
+    }
+
+    synchronized static private void loadProps(){
+        log.info("开始加载yml文件内容.......");
+        props = new Properties();
+        InputStream in = null;
+        try {
+            // 通过类加载器进行获取properties文件流
+            in = PropertiesUtil.class.getClassLoader().getResourceAsStream("application-dev.yml");
+            props.load(in);
+        } catch (FileNotFoundException e) {
+            log.error("文件未找到");
+        } catch (IOException e) {
+            log.error("出现IOException");
+        } finally {
+            try {
+                if(null != in) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                log.error("文件流关闭出现异常");
+            }
+        }
+        log.info("加载yml文件内容完成...........");
+        log.info("yml文件内容：" + props);
+    }
+
+    public static String getProperty(String key){
+        if(null == props) {
+            loadProps();
+        }
+        return props.getProperty(key);
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+        if(null == props) {
+            loadProps();
+        }
+        return props.getProperty(key, defaultValue);
+    }
+
+    /**
+     * 将json格式的字符串转换成对应的对象
+     * @param propertyKey
+     * @return
+     */
+    public static String[][] getPropertyVal(String propertyKey){
+
+        String appGroups = getProperty(propertyKey);
+
+        Gson gson = new Gson();
+
+        String[][] appGroup =  gson.fromJson(appGroups,new TypeToken<String[][]>(){}.getType());
+        //String[][] appGroup =  gson.fromJson(appGroups,String[][].class);
+
+       return  appGroup;
+    }
+
+
+}
